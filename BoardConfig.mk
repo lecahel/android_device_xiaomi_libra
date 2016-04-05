3949edbacee2f294af3b2a2eabfe90eb0ce1719a
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-04-05 12:13:31 +0700

    libra: Initial TWRP config
    Change-Id: I9de8343988eddfd1c59aab594b33e81e6861bb01

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,4 +185,15 @@ BOARD_HARDWARE_CLASS += device/xiaomi/libra/cmhw
 # Disable Jack & Jill compilation
 ANDROID_COMPILE_WITH_JACK := false
 
+# TWRP
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
 -include vendor/xiaomi/libra/BoardConfigVendor.mk
